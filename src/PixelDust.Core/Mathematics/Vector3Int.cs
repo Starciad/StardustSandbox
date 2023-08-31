d654a625b564fff24d70ecfaa050835817312a0d
Starciad <davigamerlsf16@gmail.com>
2023-08-31 14:11:20 -0300

    Removal of useless methods from integer vector classes.

--- src/PixelDust.Core/Mathematics/Vector3Int.cs
+++ src/PixelDust.Core/Mathematics/Vector3Int.cs
@@ -136,15 +136,46 @@ namespace PixelDust.Core.Mathematics
             value1.X += value2.X;
             value1.Y += value2.Y;
             value1.Z += value2.Z;
+
             return value1;
         }
-        public static Vector3Int Barycentric(Vector3Int value1, Vector3Int value2, Vector3Int value3, int amount1, int amount2)
+        public static Vector3Int Subtract(Vector3Int value1, Vector3Int value2)
         {
-            return new Vector3Int(
-                (int)MathHelper.Barycentric(value1.X, value2.X, value3.X, amount1, amount2),
-                (int)MathHelper.Barycentric(value1.Y, value2.Y, value3.Y, amount1, amount2),
-                (int)MathHelper.Barycentric(value1.Z, value2.Z, value3.Z, amount1, amount2));
+            value1.X -= value2.X;
+            value1.Y -= value2.Y;
+            value1.Z -= value2.Z;
+            return value1;
         }
+        public static Vector3Int Multiply(Vector3Int value1, Vector3Int value2)
+        {
+            value1.X *= value2.X;
+            value1.Y *= value2.Y;
+            value1.Z *= value2.Z;
+            return value1;
+        }
+        public static Vector3Int Multiply(Vector3Int value1, int scaleFactor)
+        {
+            value1.X *= scaleFactor;
+            value1.Y *= scaleFactor;
+            value1.Z *= scaleFactor;
+            return value1;
+        }
+        public static Vector3Int Divide(Vector3Int value1, Vector3Int value2)
+        {
+            value1.X /= value2.X;
+            value1.Y /= value2.Y;
+            value1.Z /= value2.Z;
+            return value1;
+        }
+        public static Vector3Int Divide(Vector3Int value1, int divider)
+        {
+            int factor = 1 / divider;
+            value1.X *= factor;
+            value1.Y *= factor;
+            value1.Z *= factor;
+            return value1;
+        }
+
         public static Vector3Int CatmullRom(Vector3Int value1, Vector3Int value2, Vector3Int value3, Vector3Int value4, int amount)
         {
             return new Vector3Int(
@@ -176,85 +207,22 @@ namespace PixelDust.Core.Mathematics
         {
             return Cross(vector1, vector2);
         }
-        public static int Distance(Vector3Int value1, Vector3Int value2)
+        public static float Distance(Vector3Int value1, Vector3Int value2)
         {
-            return (int)MathF.Sqrt(DistanceSquared(value1, value2));
+            return MathF.Sqrt(DistanceSquared(value1, value2));
         }
-        public static int DistanceSquared(Vector3Int value1, Vector3Int value2)
+        public static float DistanceSquared(Vector3Int value1, Vector3Int value2)
         {
             return (value1.X - value2.X) * (value1.X - value2.X) +
                    (value1.Y - value2.Y) * (value1.Y - value2.Y) +
                    (value1.Z - value2.Z) * (value1.Z - value2.Z);
         }
-        public static Vector3Int Divide(Vector3Int value1, Vector3Int value2)
-        {
-            value1.X /= value2.X;
-            value1.Y /= value2.Y;
-            value1.Z /= value2.Z;
-            return value1;
-        }
-        public static Vector3Int Divide(Vector3Int value1, int divider)
-        {
-            int factor = 1 / divider;
-            value1.X *= factor;
-            value1.Y *= factor;
-            value1.Z *= factor;
-            return value1;
-        }
+
         public static int Dot(Vector3Int value1, Vector3Int value2)
         {
             return value1.X * value2.X + value1.Y * value2.Y + value1.Z * value2.Z;
         }
 
-        public readonly override bool Equals(object obj)
-        {
-            if (obj is not Vector3Int)
-                return false;
-
-            Vector3Int other = (Vector3Int)obj;
-            return X == other.X &&
-                    Y == other.Y &&
-                    Z == other.Z;
-        }
-        public readonly bool Equals(Vector3Int other)
-        {
-            return X == other.X &&
-                    Y == other.Y &&
-                    Z == other.Z;
-        }
-        public readonly override int GetHashCode()
-        {
-            return HashCode.Combine(X, Y, Z);
-        }
-
-        public static Vector3Int Hermite(Vector3Int value1, Vector3Int tangent1, Vector3Int value2, Vector3Int tangent2, int amount)
-        {
-            return new Vector3Int((int)MathHelper.Hermite(value1.X, tangent1.X, value2.X, tangent2.X, amount),
-                               (int)MathHelper.Hermite(value1.Y, tangent1.Y, value2.Y, tangent2.Y, amount),
-                               (int)MathHelper.Hermite(value1.Z, tangent1.Z, value2.Z, tangent2.Z, amount));
-        }
-        public readonly int Length()
-        {
-            return (int)MathF.Sqrt((X * X) + (Y * Y) + (Z * Z));
-        }
-        public readonly int LengthSquared()
-        {
-            return (X * X) + (Y * Y) + (Z * Z);
-        }
-        public static Vector3Int Lerp(Vector3Int value1, Vector3Int value2, int amount)
-        {
-            return new Vector3Int(
-                (int)MathHelper.Lerp(value1.X, value2.X, amount),
-                (int)MathHelper.Lerp(value1.Y, value2.Y, amount),
-                (int)MathHelper.Lerp(value1.Z, value2.Z, amount));
-        }
-        public static Vector3Int LerpPrecise(Vector3Int value1, Vector3Int value2, int amount)
-        {
-            return new Vector3Int(
-                (int)MathHelper.LerpPrecise(value1.X, value2.X, amount),
-                (int)MathHelper.LerpPrecise(value1.Y, value2.Y, amount),
-                (int)MathHelper.LerpPrecise(value1.Z, value2.Z, amount));
-        }
         public static Vector3Int Max(Vector3Int value1, Vector3Int value2)
         {
             return new Vector3Int(
@@ -269,68 +237,20 @@ namespace PixelDust.Core.Mathematics
                 MathHelper.Min(value1.Y, value2.Y),
                 MathHelper.Min(value1.Z, value2.Z));
         }
-        public static Vector3Int Multiply(Vector3Int value1, Vector3Int value2)
-        {
-            value1.X *= value2.X;
-            value1.Y *= value2.Y;
-            value1.Z *= value2.Z;
-            return value1;
-        }
-        public static Vector3Int Multiply(Vector3Int value1, int scaleFactor)
-        {
-            value1.X *= scaleFactor;
-            value1.Y *= scaleFactor;
-            value1.Z *= scaleFactor;
-            return value1;
-        }
+
         public static Vector3Int Negate(Vector3Int value)
         {
             value = new Vector3Int(-value.X, -value.Y, -value.Z);
             return value;
         }
-        public void Normalize()
-        {
-            float factor = MathF.Sqrt((X * X) + (Y * Y) + (Z * Z));
-            factor = 1f / factor;
-            X *= (int)MathF.Round(factor);
-            Y *= (int)MathF.Round(factor);
-            Z *= (int)MathF.Round(factor);
-        }
-        public static Vector3Int Normalize(Vector3Int value)
-        {
-            float factor = MathF.Sqrt((value.X * value.X) + (value.Y * value.Y) + (value.Z * value.Z));
-            factor = 1f / factor;
-            int iFactor = (int)MathF.Round(factor);
 
-            return new Vector3Int(value.X * iFactor, value.Y * iFactor, value.Z * iFactor);
-        }
-        public static Vector3Int Reflect(Vector3Int vector, Vector3Int normal)
+        public readonly int Length()
         {
-            // I is the original array
-            // N is the normal of the incident plane
-            // R = I - (2 * N * ( DotProduct[ I,N] ))
-            Vector3Int reflectedVector;
-            // inline the dotProduct here instead of calling method
-            int dotProduct = ((vector.X * normal.X) + (vector.Y * normal.Y)) + (vector.Z * normal.Z);
-            reflectedVector.X = (int)(vector.X - (2.0f * normal.X) * dotProduct);
-            reflectedVector.Y = (int)(vector.Y - (2.0f * normal.Y) * dotProduct);
-            reflectedVector.Z = (int)(vector.Z - (2.0f * normal.Z) * dotProduct);
-
-            return reflectedVector;
+            return (int)MathF.Sqrt((X * X) + (Y * Y) + (Z * Z));
         }
-        public static Vector3Int SmoothStep(Vector3Int value1, Vector3Int value2, int amount)
+        public readonly int LengthSquared()
         {
-            return new Vector3Int(
-                (int)MathHelper.SmoothStep(value1.X, value2.X, amount),
-                (int)MathHelper.SmoothStep(value1.Y, value2.Y, amount),
-                (int)MathHelper.SmoothStep(value1.Z, value2.Z, amount));
-        }
-        public static Vector3Int Subtract(Vector3Int value1, Vector3Int value2)
-        {
-            value1.X -= value2.X;
-            value1.Y -= value2.Y;
-            value1.Z -= value2.Z;
-            return value1;
+            return (X * X) + (Y * Y) + (Z * Z);
         }
 
         public readonly void Deconstruct(out int x, out int y, out int z)
@@ -344,10 +264,17 @@ namespace PixelDust.Core.Mathematics
 
         #region Operators
 
-        public static implicit operator Vector3Int(System.Numerics.Vector3 value)
+        // Implicit
+        public static implicit operator Vector3Int(Vector3 value)
         {
             return new Vector3Int((int)value.X, (int)value.Y, (int)value.Z);
         }
+        public static explicit operator Vector3(Vector3Int value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+
+        // Equals
         public static bool operator ==(Vector3Int value1, Vector3Int value2)
         {
             return value1.X == value2.X
@@ -358,6 +285,8 @@ namespace PixelDust.Core.Mathematics
         {
             return !(value1 == value2);
         }
+
+        // Calc
         public static Vector3Int operator +(Vector3Int value1, Vector3Int value2)
         {
             value1.X += value2.X;
@@ -418,10 +347,6 @@ namespace PixelDust.Core.Mathematics
 
         #region Conversions
 
-        public readonly System.Numerics.Vector3 ToNumerics()
-        {
-            return new System.Numerics.Vector3(X, Y, Z);
-        }
         public readonly override string ToString()
         {
             StringBuilder sb = new(32);
@@ -435,6 +360,29 @@ namespace PixelDust.Core.Mathematics
             return sb.ToString();
         }
 
+        #endregion
+
+        #region Override
+        public readonly override bool Equals(object obj)
+        {
+            if (obj is not Vector3Int)
+                return false;
+
+            Vector3Int other = (Vector3Int)obj;
+            return X == other.X &&
+                    Y == other.Y &&
+                    Z == other.Z;
+        }
+        public readonly bool Equals(Vector3Int other)
+        {
+            return X == other.X &&
+                    Y == other.Y &&
+                    Z == other.Z;
+        }
+        public readonly override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
         #endregion
     }
 }
