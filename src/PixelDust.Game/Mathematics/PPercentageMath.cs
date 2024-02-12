beedbd9e25a1d9b4505fc281e69a5f5e2cf51211
Starciad <davigamerlsf16@gmail.com>
2024-02-12 14:35:49 -0300

    feat: Add math utility for percentages.

---
+++ src/PixelDust.Game/Mathematics/PPercentageMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PixelDust.Game.Mathematics
+{
+    public static class PPercentageMath
+    {
+        public static float PercentageOfValue(float value, float percentage)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percentage), "Percentage must be between 1 and 100.");
+            }
+
+            return (value * percentage) / 100f;
+        }
+
+        public static float PercentageFromValue(float total, float partial)
+        {
+            if (total == 0)
+            {
+                throw new ArgumentException("Total value cannot be zero.", nameof(total));
+            }
+
+            return (partial / total) * 100f;
+        }
+    }
+}
