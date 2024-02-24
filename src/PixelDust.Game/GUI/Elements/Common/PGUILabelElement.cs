c00491b09b559c353d619d2ed5181a0b0accd33c
Starciad <davigamerlsf16@gmail.com>
2024-02-24 15:14:08 -0300

    feat: Add label element.

---
+++ src/PixelDust.Game/GUI/Elements/Common/PGUILabelElement.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using System.Text;
+
+namespace PixelDust.Game.GUI.Elements.Common
+{
+    public class PGUILabelElement : PGUIElement
+    {
+        private readonly StringBuilder textContentStringBuilder = new();
+
+        private SpriteFont textFont;
+        private Color textColor;
+        private float textRotation;
+        private Vector2 textScale;
+
+        public PGUILabelElement()
+        {
+            this.IsVisible = true;
+        }
+
+        protected override void OnDraw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(this.textFont, this.textContentStringBuilder, this.Position, this.textColor, this.textRotation, GetOrigin(), this.textScale, SpriteEffects.None, 0f);
+        }
+
+        public void SetTextContent(string value)
+        {
+            this.textContentStringBuilder.Clear();
+            this.textContentStringBuilder.Append(value);
+        }
+
+        public void SetFontFamily(string fontFamilyName)
+        {
+            textFont = Game.AssetDatabase.GetFont(fontFamilyName);
+        }
+
+        public void SetColor(Color color)
+        {
+            this.textColor = color;
+        }
+
+        public void SetRotation(float rotation)
+        {
+            this.textRotation = rotation;
+        }
+
+        private static Vector2 GetOrigin()
+        {
+            return Vector2.Zero;
+        }
+    }
+}
