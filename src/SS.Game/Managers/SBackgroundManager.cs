72b8031bdc223ba0fc23a8607d2706409433dc00
Starciad <davigamerlsf16@gmail.com>
2024-10-20 00:18:50 -0300

    feat: adjust parallax.

--- src/SS.Game/Managers/SBackgroundManager.cs
+++ src/SS.Game/Managers/SBackgroundManager.cs
@@ -13,7 +13,11 @@ namespace StardustSandbox.Game.Managers
         public override void Initialize()
         {
             this.background01 = new(this.SGameInstance, this.SGameInstance.AssetDatabase.GetTexture("background_1"));
-            this.background01.AddLayer(new Point(0, 0), new Vector2(30f), false, false);
+            this.background01.AddLayer(new Point(0, 0), new Vector2(0f), true, true);
+            this.background01.AddLayer(new Point(0, 1), new Vector2(5f), false, true);
+            this.background01.AddLayer(new Point(0, 2), new Vector2(8f), false, true);
+            this.background01.AddLayer(new Point(0, 3), new Vector2(10f), false, true);
+            this.background01.AddLayer(new Point(0, 4), new Vector2(15f), false, true);
         }
 
         public override void Update(GameTime gameTime)
