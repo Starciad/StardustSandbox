4d20d49ab3db4722d906ac8ede8a576c824e198d
Starciad <davigamerlsf16@gmail.com>
2024-11-30 00:33:44 -0300

    feat: add main menu buttons.

--- src/SS.ContentBundle/GUISystem/Menus/SGUI_MainMenu.Designer.cs
+++ src/SS.ContentBundle/GUISystem/Menus/SGUI_MainMenu.Designer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 
+using StardustSandbox.Core.Constants;
 using StardustSandbox.Core.Enums.General;
 using StardustSandbox.Core.GUISystem.Elements;
 using StardustSandbox.Core.GUISystem.Elements.Graphics;
@@ -10,22 +11,88 @@ namespace StardustSandbox.ContentBundle.GUISystem.Menus
 {
     public sealed partial class SGUI_MainMenu
     {
+        private enum SMainMenuButtonIndex : byte
+        {
+            Create = 0,
+            Play = 1,
+            Options = 2,
+            Quit = 3
+        }
+
         private ISGUILayoutBuilder layout;
         private SGUIRootElement rootElement;
+
+        private (SGUISliceImageElement background, SGUILabelElement label)[] menuButtons;
 
         protected override void OnBuild(ISGUILayoutBuilder layout)
         {
             this.layout = layout;
             this.rootElement = layout.RootElement;
             
+            // [ CREATING ]
+            // TITLE
             SGUIImageElement gameTitle = new(this.SGameInstance);
+
+            // BUTTONS
+            this.menuButtons = [
+                (new(this.SGameInstance), new(this.SGameInstance)),
+                (new(this.SGameInstance), new(this.SGameInstance)),
+                (new(this.SGameInstance), new(this.SGameInstance)),
+                (new(this.SGameInstance), new(this.SGameInstance))
+            ];
+
+            // [ SETTINGS ]
+            // Title
             gameTitle.SetTexture(this.gameTitleTexture);
             gameTitle.SetOriginPivot(SCardinalDirection.Northwest);
             gameTitle.SetScale(new Vector2(2));
-            gameTitle.SetPositionAnchor(SCardinalDirection.West);
+            gameTitle.SetPositionAnchor(SCardinalDirection.Northwest);
             gameTitle.SetSize(new SSize2(292, 112));
+            gameTitle.SetMargin(new Vector2(32, 0));
+            gameTitle.PositionRelativeToElement(this.rootElement);
 
+            // BUTTONS
+            // Backgrounds
+            Vector2 baseMargin = new(96, -64);
+            int spacing = 110;
+
+            for (int i = 0; i < this.menuButtons.Length; i++)
+            {
+                this.menuButtons[i].background.SetTexture(this.buttonBackgroundTexture);
+                this.menuButtons[i].background.SetScale(new Vector2(8, 1f));
+                this.menuButtons[i].background.SetPositionAnchor(SCardinalDirection.West);
+                this.menuButtons[i].background.SetMargin(baseMargin);
+                this.menuButtons[i].background.SetColor(Color.White);
+                this.menuButtons[i].background.PositionRelativeToElement(this.rootElement);
+                baseMargin.Y += spacing;
+            }
+
+            // Labels
+            this.menuButtons[(byte)SMainMenuButtonIndex.Create].label.SetTextContent("Create");
+            this.menuButtons[(byte)SMainMenuButtonIndex.Play].label.SetTextContent("Play");
+            this.menuButtons[(byte)SMainMenuButtonIndex.Options].label.SetTextContent("Options");
+            this.menuButtons[(byte)SMainMenuButtonIndex.Quit].label.SetTextContent("Quit");
+
+            for (int i = 0; i < this.menuButtons.Length; i++)
+            {
+                this.menuButtons[i].label.SetScale(new Vector2(0.15f));
+                this.menuButtons[i].label.SetMargin(new Vector2(18, -16));
+                this.menuButtons[i].label.SetColor(new Color(206, 214, 237, 255));
+                this.menuButtons[i].label.SetFontFamily(SFontFamilyConstants.BIG_APPLE_3PM);
+                this.menuButtons[i].label.SetBorders(true);
+                this.menuButtons[i].label.SetBordersColor(new Color(45, 53, 74, 255));
+                this.menuButtons[i].label.SetBorderOffset(new Vector2(4.4f));
+                this.menuButtons[i].label.PositionRelativeToElement(this.menuButtons[i].background);
+            }
+
+            // ADDING
             layout.AddElement(gameTitle);
+
+            for (int i = 0; i < this.menuButtons.Length; i++)
+            {
+                layout.AddElement(this.menuButtons[i].background);
+                layout.AddElement(this.menuButtons[i].label);
+            }
         }
     }
 }
--- src/SS.ContentBundle/GUISystem/Menus/SGUI_MainMenu.cs
+++ src/SS.ContentBundle/GUISystem/Menus/SGUI_MainMenu.cs
@@ -1,19 +1,54 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
+using StardustSandbox.Core.Constants.GUI.Common;
+using StardustSandbox.Core.Constants.GUI;
 using StardustSandbox.Core.GUISystem;
+using StardustSandbox.Core.GUISystem.Elements.Graphics;
 using StardustSandbox.Core.GUISystem.Events;
 using StardustSandbox.Core.Interfaces.General;
 using StardustSandbox.Core.Interfaces.GUI;
+using StardustSandbox.Core.Mathematics.Primitives;
+using StardustSandbox.Core.GUISystem.Elements;
 
 namespace StardustSandbox.ContentBundle.GUISystem.Menus
 {
     public sealed partial class SGUI_MainMenu : SGUISystem
     {
         private readonly Texture2D gameTitleTexture;
+        private readonly Texture2D buttonBackgroundTexture;
 
         public SGUI_MainMenu(ISGame gameInstance, string identifier, SGUIEvents guiEvents) : base(gameInstance, identifier, guiEvents)
         {
             this.gameTitleTexture = gameInstance.AssetDatabase.GetTexture("game_title_1");
+            this.buttonBackgroundTexture = gameInstance.AssetDatabase.GetTexture("gui_background_2");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            // Individually check all element slots present in the item catalog.
+            for (int i = 0; i < this.menuButtons.Length; i++)
+            {
+                (SGUISliceImageElement buttonBackground, SGUILabelElement buttonLabel) = this.menuButtons[i];
+
+                if (!buttonBackground.IsVisible)
+                {
+                    continue;
+                }
+
+                // Highlight when mouse is over slot.
+                if (this.GUIEvents.OnMouseOver(buttonBackground.Position, buttonBackground.Size))
+                {
+                    buttonBackground.SetColor(Color.DarkGray);
+                }
+                // If none of the above events occur, the slot continues with its normal color.
+                else
+                {
+                    buttonBackground.SetColor(Color.White);
+                }
+            }
         }
     }
 }
