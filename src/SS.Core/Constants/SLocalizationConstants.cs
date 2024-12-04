d63c1dfb9a03ea54f36965860dee2dbae583e1d9
Starciad <davigamerlsf16@gmail.com>
2024-12-04 17:17:58 -0300

    feat: add .resx files for localization.

---
+++ src/SS.Core/Constants/SLocalizationConstants.cs
@@ -0,0 +1,14 @@
+﻿using StardustSandbox.Core.Localization;
+
+namespace StardustSandbox.Core.Constants
+{
+    public static class SLocalizationConstants
+    {
+        public static SGameCulture DefaultCulture => Cultures[0];
+
+        public static SGameCulture[] Cultures => [
+            new("en", "US"),
+            new("pt", "BR"),
+        ];
+    }
+}
---
+++ src/SS.Core/Localization/SGameCulture.cs
@@ -0,0 +1,9 @@
+﻿namespace StardustSandbox.Core.Localization
+{
+    public sealed class SGameCulture(string language, string region)
+    {
+        public string Name => string.Concat(this.Language, '-', this.Region);
+        public string Language => language.ToLowerInvariant();
+        public string Region => region.ToUpperInvariant();
+    }
+}
