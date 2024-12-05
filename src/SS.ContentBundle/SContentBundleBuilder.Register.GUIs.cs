529516118c9934197f08f063946b9fa977346ea4
Starciad <davigamerlsf16@gmail.com>
2024-12-05 17:03:34 -0300

    feat: add experimental GUI.

--- src/SS.ContentBundle/SContentBundleBuilder.Register.GUIs.cs
+++ src/SS.ContentBundle/SContentBundleBuilder.Register.GUIs.cs
@@ -1,5 +1,4 @@
-﻿using StardustSandbox.ContentBundle.GUISystem.GUIs.Debug;
-using StardustSandbox.ContentBundle.GUISystem.GUIs.Hud;
+﻿using StardustSandbox.ContentBundle.GUISystem.GUIs.Hud;
 using StardustSandbox.ContentBundle.GUISystem.GUIs.Hud.Complements;
 using StardustSandbox.ContentBundle.GUISystem.GUIs.Menus;
 using StardustSandbox.Core.Constants.GUI;
@@ -12,18 +11,13 @@ namespace StardustSandbox.ContentBundle
     {
         protected override void OnRegisterGUIs(ISGame game, SGUIDatabase guiDatabase)
         {
-            SGUI_TextDebug textDebug = new(game, SGUIConstants.MAIN_MENU_IDENTIFIER, game.GUIManager.GUIEvents);
-
             SGUI_MainMenu mainMenu = new(game, SGUIConstants.MAIN_MENU_IDENTIFIER, game.GUIManager.GUIEvents);
             SGUI_OptionsMenu optionsMenu = new(game, SGUIConstants.OPTIONS_MENU_IDENTIFIER, game.GUIManager.GUIEvents);
 
             SGUI_HUD hud = new(game, SGUIConstants.HUD_IDENTIFIER, game.GUIManager.GUIEvents);
             SGUI_ItemExplorer itemExplorer = new(game, SGUIConstants.HUD_ELEMENT_EXPLORER_IDENTIFIER, game.GUIManager.GUIEvents, hud);
 
-            // [DEBUG]
-            guiDatabase.RegisterGUISystem(SGUIConstants.MAIN_MENU_IDENTIFIER, textDebug);
-
-            // guiDatabase.RegisterGUISystem(SGUIConstants.MAIN_MENU_IDENTIFIER, mainMenu);
+            guiDatabase.RegisterGUISystem(SGUIConstants.MAIN_MENU_IDENTIFIER, mainMenu);
             guiDatabase.RegisterGUISystem(SGUIConstants.OPTIONS_MENU_IDENTIFIER, optionsMenu);
             guiDatabase.RegisterGUISystem(SGUIConstants.HUD_IDENTIFIER, hud);
             guiDatabase.RegisterGUISystem(SGUIConstants.HUD_ELEMENT_EXPLORER_IDENTIFIER, itemExplorer);
