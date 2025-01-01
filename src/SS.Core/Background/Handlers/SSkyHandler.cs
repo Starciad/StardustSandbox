3628374415b8cdc8b5ab9610490465a30ccba7c0
Starciad <davigamerlsf16@gmail.com>
2025-01-01 19:06:46 -0300

    feat: remove transparency from background elements.

--- src/SS.Core/Background/Handlers/SSkyHandler.cs
+++ src/SS.Core/Background/Handlers/SSkyHandler.cs
@@ -89,64 +89,64 @@ namespace StardustSandbox.Core.Background.Handlers
             {
                 StartTime = new(0, 0, 0), // Midnight
                 EndTime = new(3, 0, 0),  // Late Night
-                Color1 = (new Color(60, 40, 90, 200), new Color(30, 30, 60, 180)),
-                Color2 = (new Color(30, 30, 60, 180), new Color(20, 25, 50, 170)),
+                Color1 = (new Color(60, 40, 90, 255), new Color(30, 30, 60, 255)),
+                Color2 = (new Color(30, 30, 60, 255), new Color(20, 25, 50, 255)),
             },
         
             new()
             {
                 StartTime = new(3, 0, 0), // Late Night
                 EndTime = new(6, 0, 0),  // Dawn
-                Color1 = (new Color(30, 30, 60, 180), new Color(20, 25, 50, 170)),
-                Color2 = (new Color(70, 80, 100, 220), new Color(100, 70, 50, 200)),
+                Color1 = (new Color(30, 30, 60, 255), new Color(20, 25, 50, 255)),
+                Color2 = (new Color(70, 80, 100, 255), new Color(100, 70, 50, 255)),
             },
         
             new()
             {
                 StartTime = new(6, 0, 0), // Dawn
                 EndTime = new(8, 0, 0),  // Early Morning
-                Color1 = (new Color(70, 80, 100, 220), new Color(100, 70, 50, 200)),
-                Color2 = (new Color(110, 130, 170, 240), new Color(190, 160, 120, 230)),
+                Color1 = (new Color(70, 80, 100, 255), new Color(100, 70, 50, 255)),
+                Color2 = (new Color(110, 130, 170, 255), new Color(190, 160, 120, 255)),
             },
         
             new()
             {
                 StartTime = new(8, 0, 0), // Early Morning
                 EndTime = new(12, 0, 0), // Noon
-                Color1 = (new Color(110, 130, 170, 240), new Color(190, 160, 120, 230)),
-                Color2 = (new Color(160, 200, 250, 255), new Color(220, 180, 130, 245)),
+                Color1 = (new Color(110, 130, 170, 255), new Color(190, 160, 120, 255)),
+                Color2 = (new Color(160, 200, 250, 255), new Color(220, 180, 130, 255)),
             },
         
             new()
             {
                 StartTime = new(12, 0, 0), // Noon
                 EndTime = new(15, 0, 0),  // Early Afternoon
-                Color1 = (new Color(160, 200, 250, 255), new Color(220, 180, 130, 245)),
-                Color2 = (new Color(200, 220, 250, 255), new Color(250, 200, 130, 235)),
+                Color1 = (new Color(160, 200, 250, 255), new Color(220, 180, 130, 255)),
+                Color2 = (new Color(200, 220, 250, 255), new Color(250, 200, 130, 255)),
             },
         
             new()
             {
                 StartTime = new(15, 0, 0), // Early Afternoon
                 EndTime = new(18, 0, 0),  // Dusk
-                Color1 = (new Color(200, 220, 250, 255), new Color(250, 200, 130, 235)),
-                Color2 = (new Color(220, 130, 100, 220), new Color(120, 70, 50, 200)),
+                Color1 = (new Color(200, 220, 250, 255), new Color(250, 200, 130, 255)),
+                Color2 = (new Color(220, 130, 100, 255), new Color(120, 70, 50, 255)),
             },
         
             new()
             {
                 StartTime = new(18, 0, 0), // Dusk
                 EndTime = new(20, 0, 0), // Evening
-                Color1 = (new Color(220, 130, 100, 220), new Color(120, 70, 50, 200)),
-                Color2 = (new Color(80, 40, 50, 180), new Color(40, 30, 60, 160)),
+                Color1 = (new Color(220, 130, 100, 255), new Color(120, 70, 50, 255)),
+                Color2 = (new Color(80, 40, 50, 255), new Color(40, 30, 60, 255)),
             },
         
             new()
             {
                 StartTime = new(20, 0, 0), // Evening
                 EndTime = new(23, 59, 59), // Midnight
-                Color1 = (new Color(80, 40, 50, 180), new Color(40, 30, 60, 160)),
-                Color2 = (new Color(60, 40, 90, 200), new Color(30, 30, 60, 180)),
+                Color1 = (new Color(80, 40, 50, 255), new Color(40, 30, 60, 255)),
+                Color2 = (new Color(60, 40, 90, 255), new Color(30, 30, 60, 255)),
             },
         ];
 
