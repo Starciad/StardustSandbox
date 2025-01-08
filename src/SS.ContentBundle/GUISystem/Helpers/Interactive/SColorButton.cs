085e8dd7b7a00072a5c6beb52b2a672446d756cd
Starciad <davigamerlsf16@gmail.com>
2025-01-08 12:12:57 -0300

    feat: add color buttons.

---
+++ src/SS.ContentBundle/GUISystem/Helpers/Interactive/SColorButton.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+using System;
+
+namespace StardustSandbox.ContentBundle.GUISystem.Helpers.Interactive
+{
+    internal sealed class SColorButton(string displayName, Color color)
+    {
+        internal string DisplayName => displayName;
+        internal Color Color => color;
+    }
+}
