3e8563612915bc31db3631104260a17645c3cd71
Starciad <davigamerlsf16@gmail.com>
2025-01-23 15:48:14 -0300

    fix: make gases not impede the horizontal movement of liquids.

--- src/SS.ContentBundle/Elements/Gases/SSteam.cs
+++ src/SS.ContentBundle/Elements/Gases/SSteam.cs
@@ -13,7 +13,7 @@ namespace StardustSandbox.ContentBundle.Elements.Gases
             this.referenceColor = new(171, 208, 218, 136);
             this.texture = gameInstance.AssetDatabase.GetTexture("element_19");
             this.Rendering.SetRenderingMechanism(new SElementBlobRenderingMechanism());
-            this.defaultTemperature = 122;
+            this.defaultTemperature = 200;
             this.movementType = SGasMovementType.Spread;
             this.defaultDensity = 1;
         }
--- src/SS.Core/Elements/Templates/Liquids/SLiquid.cs
+++ src/SS.Core/Elements/Templates/Liquids/SLiquid.cs
@@ -3,8 +3,10 @@
 using StardustSandbox.Core.Elements.Templates.Gases;
 using StardustSandbox.Core.Elements.Utilities;
 using StardustSandbox.Core.Enums.General;
+using StardustSandbox.Core.Extensions;
 using StardustSandbox.Core.Interfaces;
 using StardustSandbox.Core.Interfaces.Elements;
+using StardustSandbox.Core.Mathematics;
 
 namespace StardustSandbox.Core.Elements.Templates.Liquids
 {
@@ -27,7 +29,7 @@ namespace StardustSandbox.Core.Elements.Templates.Liquids
                 }
             }
 
-            SElementUtility.UpdateHorizontalPosition(this.Context, this.DefaultDispersionRate);
+            UpdateHorizontalPosition();
             this.Context.SetElementFreeFalling(this.Context.Layer, false);
         }
 
@@ -55,6 +57,50 @@ namespace StardustSandbox.Core.Elements.Templates.Liquids
             }
 
             return false;
+        }
+
+        private void UpdateHorizontalPosition()
+        {
+            Point left = GetDispersionPosition(-1);
+            Point right = GetDispersionPosition(1);
+
+            float leftDistance = SPointExtensions.Distance(this.Context.Slot.Position, left);
+            float rightDistance = SPointExtensions.Distance(this.Context.Slot.Position, right);
+
+            Point targetPosition = leftDistance == rightDistance
+                ? (SRandomMath.Chance(50, 101) ? left : right)
+                : (leftDistance > rightDistance ? left : right);
+
+            if (this.Context.IsEmptyWorldSlotLayer(targetPosition, this.Context.Layer))
+            {
+                this.Context.SetPosition(targetPosition, this.Context.Layer);
+            }
+            else
+            {
+                this.Context.SwappingElements(targetPosition, this.Context.Layer);
+            }
+        }
+
+        private Point GetDispersionPosition(int direction)
+        {
+            Point dispersionPosition = this.Context.Slot.Position;
+
+            for (int i = 0; i < this.defaultDispersionRate; i++)
+            {
+                Point nextPosition = new(dispersionPosition.X + direction, dispersionPosition.Y);
+
+                if (this.Context.IsEmptyWorldSlotLayer(nextPosition, this.Context.Layer) ||
+                    this.Context.GetElement(nextPosition, this.Context.Layer) is SLiquid or SGas)
+                {
+                    dispersionPosition = nextPosition;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return dispersionPosition;
         }
     }
 }
