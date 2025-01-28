e586f8853ef650fe7308713189e4ec4a07009715
Starciad <davigamerlsf16@gmail.com>
2025-01-28 17:54:55 -0300

    feat: update game version and project files.

--- src/SS.Core/Constants/SGameConstants.cs
+++ src/SS.Core/Constants/SGameConstants.cs
@@ -4,7 +4,7 @@ namespace StardustSandbox.Core.Constants
 {
     public static class SGameConstants
     {
-        public static Version VERSION => new(1, 2, 0, 0);
+        public static Version VERSION => new(1, 2, 2, 0);
 
         public const string TITLE = "StardustSandbox";
         public const string AUTHOR = "Davi \"Starciad\" Fernandes";
