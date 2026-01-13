8cbb0d977091bd46ebcd8d3f9fbe9f2ccd6c075d
Starciad <davigamerlsf16@gmail.com>
2026-01-13 14:53:30 -0300

    feat: include the license at the top of the files.

--- src/Game/Colors/Palettes/AAP64ColorPalette.cs
+++ src/Game/Colors/Palettes/AAP64ColorPalette.cs
@@ -1,4 +1,21 @@
-﻿// =================================================================== //
+/*
+ * Copyright (C) 2026  Davi "Starciad" Fernandes <davilsfernandes.starciad.comu@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// =================================================================== //
 //                                                                     //
 // Credits to the creator of the color palette used in the project.    //
 //                                                                     //
@@ -357,3 +374,4 @@ namespace StardustSandbox.Colors.Palettes
         #endregion
     }
 }
+
