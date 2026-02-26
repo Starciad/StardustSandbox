3a68f1506b9996a7769c5d1e0a4666a233b538a9
Starciad <davigamerlsf16@gmail.com>
2026-02-26 18:59:17 -0300

    feat: update version to '2.5.0.0'.

--- src/Core/StardustSandboxGame.cs
+++ src/Core/StardustSandboxGame.cs
@@ -19,8 +19,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 
-using SharpDX.Direct3D9;
-
 using StardustSandbox.Core.Achievements;
 using StardustSandbox.Core.Audio;
 using StardustSandbox.Core.Cameras;
